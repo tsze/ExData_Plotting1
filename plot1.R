fc6e47674d725b61cd46b0c094585f5f0e8e6412
tsze <t.szegvary@gmail.com>
2014-10-10 10:25:39 -0700

    corrections

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-data_all <- read.csv("household_power_consumption.txt",header=TRUE,sep=";")
+data_all <- read.csv("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
 data_all$Date <- strptime(data_all$Date,format="%d/%m/%Y")
 data <- data_all[data_all$Date %in% c('2007-02-01', '2007-02-02'),]
 
